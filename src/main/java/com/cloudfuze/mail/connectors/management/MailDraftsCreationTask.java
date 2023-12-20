4d6207bf59a05f9ae7cfa4a47b47218fc11601ec
Brahmaiah Gudimetla <109071930+Brahmaiah-G@users.noreply.github.com>
2023-12-20 19:59:10 +0530

    Update MailDraftsCreationTask.java

--- src/main/java/com/cloudfuze/mail/connectors/management/MailDraftsCreationTask.java
+++ src/main/java/com/cloudfuze/mail/connectors/management/MailDraftsCreationTask.java
@@ -1,4 +1,4 @@
-package com.cloudfuze.mail.connectors.management;
+package com.testing.mail.connectors.management;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,18 +10,18 @@ import java.util.Map;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
-import com.cloudfuze.mail.connectors.factory.MailServiceFactory;
-import com.cloudfuze.mail.connectors.impl.OutLookMailConnector;
-import com.cloudfuze.mail.connectors.microsoft.data.AttachmentsData;
-import com.cloudfuze.mail.dao.entities.ConnectFlags;
-import com.cloudfuze.mail.dao.entities.EmailFlagsInfo;
-import com.cloudfuze.mail.exceptions.MailMigrationException;
-import com.cloudfuze.mail.repo.entities.Clouds;
-import com.cloudfuze.mail.repo.entities.Clouds.CLOUD_NAME;
-import com.cloudfuze.mail.repo.entities.EmailInfo;
-import com.cloudfuze.mail.repo.entities.EmailWorkSpace;
-import com.cloudfuze.mail.repo.entities.EmailWorkSpace.PROCESS;
-import com.cloudfuze.mail.service.DBConnectorService;
+import com.testing.mail.connectors.factory.MailServiceFactory;
+import com.testing.mail.connectors.impl.OutLookMailConnector;
+import com.testing.mail.connectors.microsoft.data.AttachmentsData;
+import com.testing.mail.dao.entities.ConnectFlags;
+import com.testing.mail.dao.entities.EmailFlagsInfo;
+import com.testing.mail.exceptions.MailMigrationException;
+import com.testing.mail.repo.entities.Clouds;
+import com.testing.mail.repo.entities.Clouds.CLOUD_NAME;
+import com.testing.mail.repo.entities.EmailInfo;
+import com.testing.mail.repo.entities.EmailWorkSpace;
+import com.testing.mail.repo.entities.EmailWorkSpace.PROCESS;
+import com.testing.mail.service.DBConnectorService;
 
 import lombok.extern.slf4j.Slf4j;
 
